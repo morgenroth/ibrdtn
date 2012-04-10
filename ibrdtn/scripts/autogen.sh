343d99c72f8cbe45f5943eced5762d8f815e78d3
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-04-10 15:50:24 +0000

    Cleanup.

--- ibrdtn/scripts/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-cd ibrcommon
-bash autogen.sh
-cd ..
-autoheader configure.in
-autoconf --include=include
--- ibrdtn/scripts/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-make clean && make -C ibrcommon clean
-sudo make uninstall
-sudo make -C ibrcommon uninstall
-
