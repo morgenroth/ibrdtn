53ac78207ab1346a594f46ca4174a2dbf0571c3b
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-06-20 10:27:49 +0000

    Change button color.

--- android/Whisper/src/de/tubs/ibr/dtn/chat/RosterView.java
+++ android/Whisper/src/de/tubs/ibr/dtn/chat/RosterView.java
@@ -34,7 +34,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import de.tubs.ibr.dtn.chat.core.Buddy;
 import de.tubs.ibr.dtn.chat.core.Roster;
