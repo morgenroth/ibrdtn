96fafacab96cff99b6159012114f3a050f1e6a41
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-10-19 10:27:01 +0000

    Netlink testcase added.

---
+++ ibrcommon/tests/link/netlinktest.cpp
@@ -0,0 +1,39 @@
+/*
+ * netlinktest.cpp
+ *
+ *  Created on: 11.10.2012
+ *      Author: morgenro
+ */
+
+#include "link/netlinktest.h"
+#include <ibrcommon/link/LinkManager.h>
+#include <ibrcommon/net/vinterface.h>
+#include <ibrcommon/net/vaddress.h>
+#include <list>
+
+CPPUNIT_TEST_SUITE_REGISTRATION (netlinktest);
+
+void netlinktest :: setUp (void)
+{
+}
+
+void netlinktest :: tearDown (void)
+{
+}
+
+void netlinktest :: baseTest (void)
+{
+	ibrcommon::LinkManager::initialize();
+
+	ibrcommon::vinterface iface("eth0");
+//	std::list<ibrcommon::vaddress> ret = ibrcommon::LinkManager::getInstance().getAddressList(iface, ibrcommon::vaddress::SCOPE_LINKLOCAL);
+	std::list<ibrcommon::vaddress> ret = ibrcommon::LinkManager::getInstance().getAddressList(iface);
+
+	std::cout << "Addresses:" << std::endl;
+	for (std::list<ibrcommon::vaddress>::iterator iter = ret.begin(); iter != ret.end(); iter++)
+	{
+		std::cout << " " << (*iter).toString() << std::endl;
+	}
+
+	//::sleep(560);
+}
---
+++ ibrcommon/tests/link/netlinktest.h
@@ -0,0 +1,30 @@
+/*
+ * netlinktest.h
+ *
+ *  Created on: 11.10.2012
+ *      Author: morgenro
+ */
+
+#ifndef NETLINKTEST_H_
+#define NETLINKTEST_H_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace std;
+
+class netlinktest : public CPPUNIT_NS :: TestFixture
+{
+	CPPUNIT_TEST_SUITE (netlinktest);
+	CPPUNIT_TEST (baseTest);
+	CPPUNIT_TEST_SUITE_END ();
+
+	public:
+		void setUp (void);
+		void tearDown (void);
+
+	protected:
+		void baseTest (void);
+};
+
+#endif /* NETLINKTEST_H_ */
