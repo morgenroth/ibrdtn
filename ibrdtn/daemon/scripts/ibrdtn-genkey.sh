b3739d1f79af49aeef7cc25d707f74cf4a18fc14
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-10-24 07:25:25 +0000

    Moved genkey script into the scripts folder.

--- ibrdtn/daemon/scripts/ibrdtn-genkey.sh
+++ ibrdtn/daemon/scripts/ibrdtn-genkey.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #
 
-trim() { echo $1; }
+if [ -z "${1}" ]; then
+  echo "Usage: ${0} <EID>"
+  exit 1
+fi
 
-HEXNAME="$(trim `echo -n $1 | hexdump -e '8/1 "%02x"'`)"
+trim() { echo ${1}; }
 
-echo "Generate key for $1"
+HEXNAME="$(trim `echo -n ${1} | hexdump -e '8/1 "%02x"'`)"
+
+echo "Generate key for ${1}"
 echo "Filename: ${HEXNAME}.pem"
 
 openssl genrsa -out ${HEXNAME}.pem 2048
 openssl rsa -in ${HEXNAME}.pem -pubout -out ${HEXNAME}.pub
 cat ${HEXNAME}.pub >> ${HEXNAME}.pem
+
