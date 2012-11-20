65daf074ff76455dccf87bbcd4b0b125d716bbec
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-11-20 11:44:41 +0000

    Add ndk project to build the dtnd for android.
    With this project it is possible to build the IBR-DTN
    daemon with the Android Native Development Kit.
    
    Signed-off-by: Dominik Schürmann <dominik@dominikschuermann.de>

---
+++ android/ndk_build/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+JNI_PATH := $(LOCAL_PATH)
+
+# include nl-3
+include $(JNI_PATH)/nl-3/android_toolchain/jni/Android_static.mk
+
+# include openssl
+NDK_PROJECT_PATH := $(JNI_PATH)/openssl/
+include $(JNI_PATH)/openssl/Android.mk
+NDK_PROJECT_PATH := $(JNI_PATH)
+
+# include ibr dtn modules
+include $(JNI_PATH)/ibrcommon/Android.mk
+include $(JNI_PATH)/ibrdtn/Android.mk
+include $(JNI_PATH)/dtnd/Android.mk
---
+++ android/ndk_build/jni/checkout_libs.sh
@@ -0,0 +1,14 @@
+git clone git://github.com/dschuermann/libnl-3-android.git nl-3
+cd nl-3
+git fetch --tags
+git checkout origin
+git checkout 7e32da396adfe6b58b23641dacb1887f5855ff9c
+
+cd ..
+
+git clone git://github.com/guardianproject/openssl-android.git openssl
+cd openssl
+git fetch --tags
+git checkout origin
+git checkout 327836b3b18784d5da25ffe904dc37b6cf368c81
+
