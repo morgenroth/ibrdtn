410540e4c1d2eb4f5175d9dfcf3a462d0d6769f2
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2012-11-28 12:43:12 +0100

    Add pthread-win32 download commands to compile script.

--- scripts/win32-compile.sh
+++ scripts/win32-compile.sh
@@ -10,6 +10,17 @@ rm -rf ${DESTDIR}
 CROSSARCH="i686-w64-mingw32"
 STATIC_OPTS="-static-libstdc++ -static-libgcc"
 
+# install pthreads
+if [ ! -e "libs/pthreads-w32" ]; then
+    PKG_NAME="pthreads-w32-2-9-1-release"
+    PKG_URL="ftp://sourceware.org/pub/pthreads-win32/${PKG_NAME}.tar.gz"
+    mkdir -p libs/
+    cd libs
+    wget -O - "${PKG_URL}" | tar xvz
+    ln -s ${PKG_NAME} pthreads-w32
+    cd ..
+fi
+
 cd libs/pthreads-w32
 make clean
 make LFLAGS="${STATIC_OPTS}" CROSS=${CROSSARCH}- clean GC-inlined
