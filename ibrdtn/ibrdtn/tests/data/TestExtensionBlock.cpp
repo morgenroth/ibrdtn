0ae1670034a9ab070a7e75fca727991d9c8d6bf5
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-01-16 11:15:38 +0000

    Add Test for ExtensionBlock.

---
+++ ibrdtn/ibrdtn/tests/data/TestExtensionBlock.cpp
@@ -0,0 +1,69 @@
+/*
+ * TestExtensionBlock.cpp
+ *
+ *  Created on: 16.01.2013
+ *      Author: morgenro
+ */
+
+#include "data/TestExtensionBlock.h"
+#include <ibrdtn/data/Bundle.h>
+#include <ibrdtn/data/Serializer.h>
+#include <ibrcommon/data/BLOB.h>
+#include <sstream>
+
+CPPUNIT_TEST_SUITE_REGISTRATION (TestExtensionBlock);
+
+void TestExtensionBlock::setUp()
+{
+}
+
+void TestExtensionBlock::tearDown()
+{
+}
+
+void TestExtensionBlock::deserializeUnknownBlock(void)
+{
+	dtn::data::Bundle b;
+	ibrcommon::BLOB::Reference ref = ibrcommon::BLOB::create();
+	(*ref.iostream()) << "hello world";
+
+	dtn::data::PayloadBlock &payload = b.push_back(ref);
+
+	std::stringstream ss;
+	dtn::data::DefaultSerializer ds(ss);
+
+	// serialize the primary block
+	ds << (const dtn::data::PrimaryBlock&)b;
+
+	// fake unknown block
+	char type = 140;
+	ss.put(type);					// block type
+	ss << dtn::data::SDNV(0);	// block flags
+	ss << dtn::data::SDNV(5);	// block size
+	ss << "12345";
+
+	// serialize payload block
+	ds << (const dtn::data::Block&)payload;
+
+	dtn::data::DefaultDeserializer dds(ss);
+	dtn::data::Bundle dest;
+
+	// deserialize the bundle
+	dds >> dest;
+
+	// check unknown block
+	const dtn::data::ExtensionBlock &unknown = dynamic_cast<const dtn::data::ExtensionBlock&>(dest.getBlock(0));
+	CPPUNIT_ASSERT_EQUAL((char)140, unknown.getType());
+	ibrcommon::BLOB::Reference uref = unknown.getBLOB();
+
+	{
+		ibrcommon::BLOB::iostream stream = uref.iostream();
+		CPPUNIT_ASSERT_EQUAL( (size_t)5, stream.size() );
+	}
+
+	// check payload block
+	dtn::data::PayloadBlock &dest_payload = dest.getBlock<dtn::data::PayloadBlock>();
+
+	CPPUNIT_ASSERT_EQUAL((size_t)11, dest_payload.getBLOB().iostream().size());
+}
+
---
+++ ibrdtn/ibrdtn/tests/data/TestExtensionBlock.h
@@ -0,0 +1,28 @@
+/*
+ * TestExtensionBlock.h
+ *
+ *  Created on: 16.01.2013
+ *      Author: morgenro
+ */
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#ifndef TESTEXTENSIONBLOCK_H_
+#define TESTEXTENSIONBLOCK_H_
+
+class TestExtensionBlock : public CPPUNIT_NS :: TestFixture
+{
+	CPPUNIT_TEST_SUITE (TestExtensionBlock);
+	CPPUNIT_TEST (deserializeUnknownBlock);
+	CPPUNIT_TEST_SUITE_END ();
+
+public:
+	void setUp (void);
+	void tearDown (void);
+
+protected:
+	void deserializeUnknownBlock(void);
+};
+
+#endif /* TESTEXTENSIONBLOCK_H_ */
