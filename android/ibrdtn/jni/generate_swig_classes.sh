4972c191d050a1073f8f441c0cc615ea1c25f5dc
Dominik Schürmann <dominik@dominikschuermann.de>
2013-04-04 13:45:09 +0200

    Remove old SWIG files, before creating new

--- android/ibrdtn/jni/generate_swig_classes.sh
+++ android/ibrdtn/jni/generate_swig_classes.sh
@@ -1,2 +1,3 @@
-mkdir ../src/de/tubs/ibr/dtn/swig
+rm -Rf ../src/de/tubs/ibr/dtn/swig
+mkdir -p ../src/de/tubs/ibr/dtn/swig
 swig -c++ -java -package de.tubs.ibr.dtn.swig -verbose -outdir ../src/de/tubs/ibr/dtn/swig/ -o android-glue/SWIGWrapper.cpp android-glue/swig.i 
