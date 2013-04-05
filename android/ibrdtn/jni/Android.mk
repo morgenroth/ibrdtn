7020ca7b3b343b5ba124df9d5d11575f39f82b24
Dominik Schürmann <dominik@dominikschuermann.de>
2013-04-05 20:24:00 +0200

    Remove old lines from Android.mk

--- android/ibrdtn/jni/Android.mk
+++ android/ibrdtn/jni/Android.mk
@@ -4,10 +4,6 @@ JNI_PATH := $(LOCAL_PATH)
 # include openssl
 include $(JNI_PATH)/openssl_Android.mk
 
-#NDK_PROJECT_PATH := $(JNI_PATH)/openssl
-#include $(JNI_PATH)/openssl/jni/Android.mk
-#NDK_PROJECT_PATH := $(JNI_PATH)
-
 # include nl-3
 include $(JNI_PATH)/nl-3/android_toolchain/jni/Android_static.mk
 
