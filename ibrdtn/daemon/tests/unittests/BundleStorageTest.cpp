1fba3fce1f470c5f9162d4d4fe6598136dd5d1d7
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-04-24 14:48:47 +0200

    daemon: Add generic BundleStorage testsuite

--- ibrdtn/daemon/tests/unittests/BundleStorageTest.cpp
+++ ibrdtn/daemon/tests/unittests/BundleStorageTest.cpp
@@ -2,7 +2,7 @@
 
 ///
 /// @file        BundleStorageTest.cpp
-/// @brief       CPPUnit-Tests for class BundleStorage
+/// @brief       CPPUnit-Tests for class SimpleBundleStorage
 /// @author      Author Name (email@mail.address)
 /// @date        Created at 2010-11-01
 /// 
@@ -33,37 +33,412 @@
 #include "BundleStorageTest.hh"
 
 
+#include <ibrdtn/data/Bundle.h>
+#include <ibrdtn/data/EID.h>
+#include <ibrcommon/thread/Thread.h>
+#include "core/TimeEvent.h"
+#include <ibrdtn/utils/Clock.h>
+#include "core/BundleCore.h"
+#include <ibrcommon/data/BLOB.h>
+#include <ibrcommon/thread/MutexLock.h>
+#include <ibrdtn/data/PayloadBlock.h>
+#include <ibrdtn/data/AgeBlock.h>
+#include "Component.h"
+
+#include "storage/SimpleBundleStorage.h"
+#include "storage/MemoryBundleStorage.h"
+
+
 
 CPPUNIT_TEST_SUITE_REGISTRATION(BundleStorageTest);
 
-/*========================== tests below ==========================*/
+/*========================== setup / teardown ==========================*/
 
-/*=== BEGIN tests for class 'BundleStorage' ===*/
-void BundleStorageTest::testRemove()
-{
-	/* test signature (const dtn::data::Bundle &b) */
-	CPPUNIT_FAIL("not implemented");
-}
-
-void BundleStorageTest::testAcceptCustody()
-{
-	/* test signature (dtn::data::Bundle &bundle) */
-	CPPUNIT_FAIL("not implemented");
-}
-
-void BundleStorageTest::testRejectCustody()
-{
-	/* test signature (const dtn::data::Bundle &bundle) */
-	CPPUNIT_FAIL("not implemented");
-}
-
-/*=== END   tests for class 'BundleStorage' ===*/
+size_t BundleStorageTest::testCounter;
+dtn::storage::BundleStorage* BundleStorageTest::_storage = NULL;
+std::vector<std::string> BundleStorageTest::_storage_names;
 
 void BundleStorageTest::setUp()
 {
+	// create a new event switch
+	esl = new ibrtest::EventSwitchLoop();
+
+	switch (testCounter++)
+	{
+	case 0:
+		{
+			// add standard memory base storage
+			_storage = new dtn::storage::MemoryBundleStorage();
+			break;
+		}
+
+	case 1:
+		{
+			// prepare path for the disk based storage
+			ibrcommon::File path("/tmp/bundle-disk-test");
+			if (path.exists()) path.remove(true);
+			ibrcommon::File::createDirectory(path);
+
+			// add disk based storage
+			_storage = new dtn::storage::SimpleBundleStorage(path);
+			break;
+		}
+	}
+
+	if (testCounter >= _storage_names.size()) testCounter = 0;
+
+	// start-up event switch
+	esl->start();
+
+	try {
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(*_storage);
+		c.initialize();
+		c.startup();
+	} catch (const bad_cast&) {
+	}
 }
 
 void BundleStorageTest::tearDown()
 {
+	dtn::core::GlobalEvent::raise(dtn::core::GlobalEvent::GLOBAL_SHUTDOWN);
+
+	try {
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(*_storage);
+		c.terminate();
+	} catch (const bad_cast&) {
+	}
+
+	esl->join();
+	delete esl;
+	esl = NULL;
+
+	// delete storage
+	delete _storage;
 }
 
+#define STORAGE_TEST(f) f(*_storage);
+
+/*========================== tests below ==========================*/
+
+void BundleStorageTest::testGetList()
+{
+	STORAGE_TEST(testGetList);
+}
+
+void BundleStorageTest::testGetList(dtn::storage::BundleStorage &storage)
+{
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+
+	storage.store(b);
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, storage.count());
+}
+
+void BundleStorageTest::testRemove()
+{
+	STORAGE_TEST(testRemove);
+}
+
+void BundleStorageTest::testRemove(dtn::storage::BundleStorage &storage)
+{
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+
+	storage.store(b);
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, storage.count());
+
+	storage.remove(b);
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+}
+
+void BundleStorageTest::testAgeBlock()
+{
+	STORAGE_TEST(testAgeBlock);
+}
+
+void BundleStorageTest::testAgeBlock(dtn::storage::BundleStorage &storage)
+{
+	dtn::data::Bundle b;
+
+	// set standard variables
+	b._source = dtn::data::EID("dtn://node-one/test");
+	b._lifetime = 1;
+	b._destination = dtn::data::EID("dtn://node-two/test");
+
+	// add some payload
+	ibrcommon::BLOB::Reference ref = ibrcommon::BLOB::create();
+	b.push_back(ref);
+
+	(*ref.iostream()) << "Hallo Welt" << std::endl;
+
+	{
+		dtn::data::AgeBlock &agebl = b.push_back<dtn::data::AgeBlock>();
+		agebl.setSeconds(42);
+
+		// store the bundle
+		storage.store(b);
+	}
+
+	dtn::data::BundleID id(b);
+
+	// clear the bundle
+	b = dtn::data::Bundle();
+
+	try {
+		// retrieve the bundle
+		CPPUNIT_ASSERT_NO_THROW( b = storage.get(id) );
+
+		// get the ageblock
+		dtn::data::AgeBlock &agebl = b.find<dtn::data::AgeBlock>();
+
+		CPPUNIT_ASSERT(agebl.getSeconds() >= 42);
+	} catch (const dtn::data::Bundle::NoSuchBlockFoundException&) {
+		CPPUNIT_FAIL("AgeBlock missing");
+	}
+}
+
+void BundleStorageTest::testClear()
+{
+	STORAGE_TEST(testClear);
+}
+
+void BundleStorageTest::testClear(dtn::storage::BundleStorage &storage)
+{
+	/* test signature () */
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+
+	storage.store(b);
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, storage.count());
+
+	storage.clear();
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+}
+
+void BundleStorageTest::testEmpty()
+{
+	STORAGE_TEST(testEmpty);
+}
+
+void BundleStorageTest::testEmpty(dtn::storage::BundleStorage &storage)
+{
+	/* test signature () */
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL(true, storage.empty());
+
+	storage.store(b);
+
+	CPPUNIT_ASSERT_EQUAL(false, storage.empty());
+
+	storage.clear();
+
+	CPPUNIT_ASSERT_EQUAL(true, storage.empty());
+}
+
+void BundleStorageTest::testCount()
+{
+	STORAGE_TEST(testCount);
+}
+
+void BundleStorageTest::testCount(dtn::storage::BundleStorage &storage)
+{
+	/* test signature () */
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)0, storage.count());
+
+	storage.store(b);
+
+	CPPUNIT_ASSERT_EQUAL((unsigned int)1, storage.count());
+}
+
+void BundleStorageTest::testSize()
+{
+	STORAGE_TEST(testSize);
+}
+
+void BundleStorageTest::testSize(dtn::storage::BundleStorage &storage)
+{
+	/* test signature () const */
+	dtn::data::Bundle b;
+	b._source = dtn::data::EID("dtn://node-one/test");
+
+	CPPUNIT_ASSERT_EQUAL((size_t)0, storage.size());
+
+	storage.store(b);
+
+	std::stringstream ss;
+	dtn::data::DefaultSerializer(ss) << b;
+
+	CPPUNIT_ASSERT_EQUAL((size_t)ss.str().length(), storage.size());
+}
+
+void BundleStorageTest::testReleaseCustody()
+{
+	STORAGE_TEST(testReleaseCustody);
+}
+
+void BundleStorageTest::testReleaseCustody(dtn::storage::BundleStorage &storage)
+{
+	/* test signature (dtn::data::BundleID &bundle) */
+	dtn::data::BundleID id;
+	dtn::data::EID eid;
+	storage.releaseCustody(eid, id);
+}
+
+void BundleStorageTest::testRaiseEvent()
+{
+	STORAGE_TEST(testRaiseEvent);
+}
+
+void BundleStorageTest::testRaiseEvent(dtn::storage::BundleStorage &storage)
+{
+		/* test signature (const Event *evt) */
+	dtn::data::EID eid("dtn://node-one/test");
+	dtn::data::Bundle b;
+	b._lifetime = 60;
+	b._source = dtn::data::EID("dtn://node-two/foo");
+
+	storage.store(b);
+	dtn::core::TimeEvent::raise(dtn::utils::Clock::getTime() + 3600, dtn::utils::Clock::getTime() + 3600 - dtn::utils::Clock::TIMEVAL_CONVERSION, dtn::core::TIME_SECOND_TICK);
+}
+
+void BundleStorageTest::testConcurrentStoreGet()
+{
+	STORAGE_TEST(testConcurrentStoreGet);
+}
+
+void BundleStorageTest::testConcurrentStoreGet(dtn::storage::BundleStorage &storage)
+{
+	class StoreProcess : public ibrcommon::JoinableThread
+	{
+	public:
+		StoreProcess(dtn::storage::BundleStorage &storage, std::list<dtn::data::Bundle> &list)
+		: _storage(storage), _list(list)
+		{ };
+
+		virtual ~StoreProcess()
+		{
+			join();
+		};
+
+		void __cancellation() throw ()
+		{
+		}
+
+	protected:
+		void run() throw ()
+		{
+			for (std::list<dtn::data::Bundle>::const_iterator iter = _list.begin(); iter != _list.end(); ++iter)
+			{
+				const dtn::data::Bundle &b = (*iter);
+				_storage.store(b);
+			}
+		}
+
+	private:
+		dtn::storage::BundleStorage &_storage;
+		std::list<dtn::data::Bundle> &_list;
+	};
+
+	// create some bundles
+	std::list<dtn::data::Bundle> list1, list2;
+
+	for (int i = 0; i < 2000; ++i)
+	{
+		dtn::data::Bundle b;
+		b._lifetime = 1;
+		b._source = dtn::data::EID("dtn://node-two/foo");
+		ibrcommon::BLOB::Reference ref = ibrcommon::BLOB::create();
+		b.push_back(ref);
+
+		(*ref.iostream()) << "Hallo Welt" << std::endl;
+
+		if ((i % 3) == 0) list1.push_back(b); else list2.push_back(b);
+	}
+
+	// store list1 in the storage
+	{ StoreProcess sp(storage, list1); sp.start(); sp.stop(); }
+
+	// store list1 in the storage
+	{
+		StoreProcess sp(storage, list2);
+		sp.start();
+
+		// in parallel we try to retrieve bundles from list1
+		for (std::list<dtn::data::Bundle>::const_iterator iter = list1.begin(); iter != list1.end(); ++iter)
+		{
+			const dtn::data::Bundle &b = (*iter);
+			dtn::data::Bundle bundle = storage.get(b);
+			storage.remove(bundle);
+		}
+
+		sp.stop();
+	}
+
+	storage.clear();
+}
+
+void BundleStorageTest::testRestore()
+{
+	STORAGE_TEST(testRestore);
+}
+
+void BundleStorageTest::testRestore(dtn::storage::BundleStorage &storage)
+{
+	// exclude memory-storage since it does not support bundle restore
+	try {
+		dynamic_cast<dtn::storage::MemoryBundleStorage&>(storage);
+		return;
+	} catch (const std::bad_cast&) {
+
+	};
+
+	try {
+		// we need control over the component to do this test
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(storage);
+
+		// create some bundles
+		for (int i = 0; i < 2000; ++i)
+		{
+			dtn::data::Bundle b;
+			b._lifetime = 1;
+			b._source = dtn::data::EID("dtn://node-two/foo");
+			ibrcommon::BLOB::Reference ref = ibrcommon::BLOB::create();
+			b.push_back(ref);
+
+			(*ref.iostream()) << "Hallo Welt" << std::endl;
+
+			storage.store(b);
+		}
+
+		// shutdown the storage
+		c.terminate();
+
+		// check if the storage is empty now
+		CPPUNIT_ASSERT_EQUAL(true, storage.empty());
+
+		// reboot the storage system
+		c.initialize();
+		c.startup();
+
+		// the storage should contain 2000 bundles
+		CPPUNIT_ASSERT_EQUAL((unsigned int)2000, storage.count());
+	} catch (const std::bad_cast&) {
+
+	};
+}
--- ibrdtn/daemon/tests/unittests/BundleStorageTest.hh
+++ ibrdtn/daemon/tests/unittests/BundleStorageTest.hh
@@ -2,8 +2,8 @@
 
 ///
 /// @file        BundleStorageTest.hh
-/// @brief       CPPUnit-Tests for class BundleStorage
-/// @author      Author Name (email@mail.address)
+/// @brief       CPPUnit-Tests for bundle storages
+/// @author      Johannes Morgenroth (morgenroth@ibr.cs.tu-bs.de)
 /// @date        Created at 2010-11-01
 /// 
 /// @version     $Revision: 2241 $
@@ -14,26 +14,75 @@
  
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include "storage/BundleStorage.h"
+#include "Component.h"
+#include "../tools/EventSwitchLoop.h"
+#include <vector>
 
 #ifndef BUNDLESTORAGETEST_HH
 #define BUNDLESTORAGETEST_HH
 class BundleStorageTest : public CppUnit::TestFixture {
 	private:
+		ibrtest::EventSwitchLoop *esl;
+
+		void testGetList(dtn::storage::BundleStorage &storage);
+		void testRemove(dtn::storage::BundleStorage &storage);
+		void testAgeBlock(dtn::storage::BundleStorage &storage);
+		void testClear(dtn::storage::BundleStorage &storage);
+		void testEmpty(dtn::storage::BundleStorage &storage);
+		void testCount(dtn::storage::BundleStorage &storage);
+		void testSize(dtn::storage::BundleStorage &storage);
+		void testReleaseCustody(dtn::storage::BundleStorage &storage);
+		void testRaiseEvent(dtn::storage::BundleStorage &storage);
+		void testConcurrentStoreGet(dtn::storage::BundleStorage &storage);
+		void testRestore(dtn::storage::BundleStorage &storage);
+
 	public:
-		/*=== BEGIN tests for class 'BundleStorage' ===*/
+#define CPPUNIT_TEST_ALL_STORAGES(testMethod) \
+		testCounter = 0; \
+		for (size_t i = 0; i < _storage_names.size(); i++) { \
+		  CPPUNIT_TEST_SUITE_ADD_TEST( \
+		    ( new CPPUNIT_NS::TestCaller<TestFixtureType>( \
+		    	context.getTestNameFor( std::string(#testMethod) + " (" + _storage_names[i] + ")" ), \
+		    	&TestFixtureType::testMethod, \
+		        context.makeFixture() ) ) ); \
+		}
+
+		void testGetList();
 		void testRemove();
-		void testAcceptCustody();
-		void testRejectCustody();
-		/*=== END   tests for class 'BundleStorage' ===*/
+		void testAgeBlock();
+		void testClear();
+		void testEmpty();
+		void testCount();
+		void testSize();
+		void testReleaseCustody();
+		void testRaiseEvent();
+		void testConcurrentStoreGet();
+		void testRestore();
 
 		void setUp();
 		void tearDown();
 
-
 		CPPUNIT_TEST_SUITE(BundleStorageTest);
-			CPPUNIT_TEST(testRemove);
-			CPPUNIT_TEST(testAcceptCustody);
-			CPPUNIT_TEST(testRejectCustody);
+
+		_storage_names.push_back("MemoryBundleStorage");
+		_storage_names.push_back("SimpleBundleStorage");
+
+		CPPUNIT_TEST_ALL_STORAGES(testGetList);
+		CPPUNIT_TEST_ALL_STORAGES(testRemove);
+		CPPUNIT_TEST_ALL_STORAGES(testAgeBlock);
+		CPPUNIT_TEST_ALL_STORAGES(testClear);
+		CPPUNIT_TEST_ALL_STORAGES(testEmpty);
+		CPPUNIT_TEST_ALL_STORAGES(testCount);
+		CPPUNIT_TEST_ALL_STORAGES(testSize);
+		CPPUNIT_TEST_ALL_STORAGES(testReleaseCustody);
+		CPPUNIT_TEST_ALL_STORAGES(testRaiseEvent);
+		CPPUNIT_TEST_ALL_STORAGES(testConcurrentStoreGet);
+		CPPUNIT_TEST_ALL_STORAGES(testRestore);
 		CPPUNIT_TEST_SUITE_END();
+
+		static size_t testCounter;
+		static dtn::storage::BundleStorage *_storage;
+		static std::vector<std::string> _storage_names;
 };
 #endif /* BUNDLESTORAGETEST_HH */
