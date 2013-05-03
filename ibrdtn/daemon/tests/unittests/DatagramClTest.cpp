4f939500feb5390b3cb0298ace4abfcd87af8832
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-05-03 11:45:30 +0200

    daemon: Add unittest for DatagramConvergenceLayer

---
+++ ibrdtn/daemon/tests/unittests/DatagramClTest.cpp
@@ -0,0 +1,120 @@
+/*
+ * DatagramClTest.cpp
+ *
+ *  Created on: 03.05.2013
+ *      Author: morgenro
+ */
+
+#include "DatagramClTest.h"
+#include "storage/MemoryBundleStorage.h"
+
+#include <ibrdtn/data/Bundle.h>
+#include <ibrdtn/data/EID.h>
+#include <ibrcommon/thread/Thread.h>
+#include "core/TimeEvent.h"
+#include <ibrdtn/utils/Clock.h>
+#include "core/BundleCore.h"
+#include <ibrcommon/data/File.h>
+#include <ibrcommon/data/BLOB.h>
+#include <ibrcommon/thread/MutexLock.h>
+#include <ibrdtn/data/PayloadBlock.h>
+#include <ibrdtn/data/AgeBlock.h>
+#include "Component.h"
+
+#include <unistd.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DatagramClTest);
+
+dtn::storage::BundleStorage* DatagramClTest::_storage = NULL;
+
+void DatagramClTest::setUp() {
+	// create a new event switch
+	_esl = new ibrtest::EventSwitchLoop();
+
+	// enable blob path
+	ibrcommon::File blob_path("/tmp/blobs");
+
+	// check if the BLOB path exists
+	if (!blob_path.exists()) {
+		// try to create the BLOB path
+		ibrcommon::File::createDirectory(blob_path);
+	}
+
+	// enable the blob provider
+	ibrcommon::BLOB::changeProvider(new ibrcommon::FileBLOBProvider(blob_path), true);
+
+	// add standard memory base storage
+	_storage = new dtn::storage::MemoryBundleStorage();
+
+	// create fake datagram service
+	_fake_service = new FakeDatagramService();
+	_fake_cl = new DatagramConvergenceLayer( _fake_service );
+
+	// initialize BundleCore
+	dtn::core::BundleCore::getInstance().initialize();
+
+	// start-up event switch
+	_esl->start();
+
+	_fake_cl->initialize();
+
+	try {
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(*_storage);
+		c.initialize();
+	} catch (const bad_cast&) {
+	}
+
+	// startup BundleCore
+	dtn::core::BundleCore::getInstance().startup();
+
+	_fake_cl->startup();
+
+	try {
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(*_storage);
+		c.startup();
+	} catch (const bad_cast&) {
+	}
+}
+
+void DatagramClTest::tearDown() {
+	_esl->stop();
+
+	_fake_cl->terminate();
+
+	try {
+		dtn::daemon::Component &c = dynamic_cast<dtn::daemon::Component&>(*_storage);
+		c.terminate();
+	} catch (const bad_cast&) {
+	}
+
+	// shutdown BundleCore
+	dtn::core::BundleCore::getInstance().terminate();
+
+	delete _fake_cl;
+	_fake_cl = NULL;
+
+	_esl->join();
+	delete _esl;
+	_esl = NULL;
+
+	// delete storage
+	delete _storage;
+}
+
+void DatagramClTest::discoveryTest() {
+	{
+		const std::set<dtn::core::Node> nodes = dtn::core::BundleCore::getInstance().getConnectionManager().getNeighbors();
+		CPPUNIT_ASSERT_EQUAL((size_t)0, nodes.size());
+	}
+
+	// send fake discovery beacon
+	_fake_service->fakeDiscovery();
+
+	// wait until the beacon has been processes
+	::usleep(500000);
+
+	{
+		const std::set<dtn::core::Node> nodes = dtn::core::BundleCore::getInstance().getConnectionManager().getNeighbors();
+		CPPUNIT_ASSERT_EQUAL((size_t)1, nodes.size());
+	}
+}
---
+++ ibrdtn/daemon/tests/unittests/FakeDatagramService.h
@@ -0,0 +1,77 @@
+/*
+ * FakeDatagramService.h
+ *
+ * Copyright (C) 2013 IBR, TU Braunschweig
+ *
+ * Written-by: Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef FAKEDATAGRAMSERVICE_H_
+#define FAKEDATAGRAMSERVICE_H_
+
+#include "net/DatagramConvergenceLayer.h"
+#include "net/DatagramService.h"
+#include <ibrcommon/thread/Queue.h>
+#include <ibrdtn/data/EID.h>
+#include <vector>
+
+class FakeDatagramService : public dtn::net::DatagramService {
+public:
+	struct Message {
+		char type;
+		char flags;
+		unsigned int seqno;
+		std::string address;
+		std::vector<char> data;
+	};
+
+	FakeDatagramService();
+	virtual ~FakeDatagramService();
+
+	void bind() throw (DatagramException);
+
+	void shutdown();
+
+	void send(const char &type, const char &flags, const unsigned int &seqno, const std::string &address, const char *buf, size_t length) throw (DatagramException);
+
+	void send(const char &type, const char &flags, const unsigned int &seqno, const char *buf, size_t length) throw (DatagramException);
+
+	size_t recvfrom(char *buf, size_t length, char &type, char &flags, unsigned int &seqno, std::string &address) throw (DatagramException);
+
+	const std::string getServiceTag() const;
+
+	const std::string getServiceDescription() const;
+
+	const ibrcommon::vinterface& getInterface() const;
+
+	dtn::core::Node::Protocol getProtocol() const;
+
+	const dtn::net::DatagramService::Parameter& getParameter() const;
+
+	void fakeDiscovery();
+
+private:
+	void genAck(const unsigned int seqno, const std::string &address);
+
+	DatagramService::Parameter _params;
+	typedef ibrcommon::Queue<Message> msg_queue;
+	msg_queue _recv_queue;
+	const ibrcommon::vinterface _iface;
+	uint16_t _discovery_sn;
+	dtn::data::EID _fake_peer;
+};
+
+#endif /* FAKEDATAGRAMSERVICE_H_ */
