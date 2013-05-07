8404dcde1f0c80d628b87619d370b42e9532c565
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-05-07 17:11:34 +0200

    daemon: [unittest] Wait until the TimeEvent has been processed

--- ibrdtn/daemon/tests/unittests/BundleStorageTest.cpp
+++ ibrdtn/daemon/tests/unittests/BundleStorageTest.cpp
@@ -32,6 +32,7 @@
 
 #include "config.h"
 #include "BundleStorageTest.hh"
+#include "../tools/TestEventListener.h"
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <ibrdtn/data/Bundle.h>
@@ -509,12 +510,16 @@ void BundleStorageTest::testExpiration(dtn::storage::BundleStorage &storage)
 	// check if the storage count is right
 	CPPUNIT_ASSERT_EQUAL((dtn::data::Size)1, storage.count());
 
+	TestEventListener<dtn::core::TimeEvent> evtl;
+
 	// raise time event to trigger expiration
 	dtn::core::TimeEvent::raise(b.timestamp + 21, dtn::utils::Clock::getUnixTimestamp(), TIME_SECOND_TICK);
 
-	// now wait until the event switch queue is empty
-	dtn::core::EventSwitch &es = dtn::core::EventSwitch::getInstance();
-	while (!es.empty()) ::sleep(1);
+	// wait until the time event has been processed
+	{
+		ibrcommon::MutexLock l(evtl.event_cond);
+		while (evtl.event_counter == 0) evtl.event_cond.wait(20000);
+	}
 
 	// special case for storages deferred mechanisms (SimpleBundleStorage)
 	// wait until all tasks of the storage are processed
