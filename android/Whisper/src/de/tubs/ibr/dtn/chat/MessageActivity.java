903ed0bd0215c1d325354d0cb0fb6c5ebb706b56
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-05-28 09:19:46 +0200

    Whisper: Fix screen orientation behavior on tablets

--- android/Whisper/src/de/tubs/ibr/dtn/chat/MessageActivity.java
+++ android/Whisper/src/de/tubs/ibr/dtn/chat/MessageActivity.java
@@ -10,26 +10,31 @@ public class MessageActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        int screenSize = (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK);
-        int orientation = getResources().getConfiguration().orientation;
+        if (savedInstanceState == null) {
+            // During initial setup, plug in the messages fragment.
+            MessageFragment messages = new MessageFragment();
+            messages.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, messages).commit();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        int screenSize = (newConfig.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK);
+        int orientation = newConfig.orientation;
 
         if (
-        		(orientation == Configuration.ORIENTATION_LANDSCAPE) &&
-        		(screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE) &&
-        		(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-        	)
+                ((orientation == Configuration.ORIENTATION_LANDSCAPE) &&
+                (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE) || (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)) &&
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+            )
         {
             // If the screen is now in landscape mode, we can show the
             // dialog in-line with the list so we don't need this activity.
             finish();
             return;
-        }
-
-        if (savedInstanceState == null) {
-            // During initial setup, plug in the messages fragment.
-        	MessageFragment messages = new MessageFragment();
-        	messages.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(android.R.id.content, messages).commit();
         }
     }
 }
