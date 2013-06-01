d48ddcb155bf74eb16e96034137811d53f22a7ba
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-06-01 09:39:14 +0200

    Talkie: unnecessary intent receiver code removed

--- android/Talkie/src/de/tubs/ibr/dtn/dtalkie/service/EventReceiver.java
+++ android/Talkie/src/de/tubs/ibr/dtn/dtalkie/service/EventReceiver.java
@@ -24,7 +24,6 @@ package de.tubs.ibr.dtn.dtalkie.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class EventReceiver extends BroadcastReceiver {
 	
@@ -32,24 +31,7 @@ public class EventReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
 		
-		Log.d("EventReceiver", "Intent received: " + action);
-		
-		if (action.equals(de.tubs.ibr.dtn.Intent.STATE))
-		{
-			String state = intent.getStringExtra("state");
-			Log.d("EventReceiver", "State: " + state);
-			if (state.equals("ONLINE"))
-			{
-				// open activity
-				Intent i = new Intent(context, TalkieService.class);
-				i.setAction(de.tubs.ibr.dtn.Intent.RECEIVE);
-				context.startService(i);
-			}
-			else if (state.equals("OFFLINE"))
-			{
-			}
-		}
-		else if (action.equals(de.tubs.ibr.dtn.Intent.RECEIVE))
+		if (action.equals(de.tubs.ibr.dtn.Intent.RECEIVE))
 		{
 			// open activity
 			Intent i = new Intent(context, TalkieService.class);
