8320abe5976c5a5ea07badb39361bdd8f2fc07f9
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-06-05 13:42:51 +0200

    ShareBox: Extend download view

--- android/ShareBox/src/de/tubs/ibr/dtn/sharebox/ui/DownloadItem.java
+++ android/ShareBox/src/de/tubs/ibr/dtn/sharebox/ui/DownloadItem.java
@@ -2,12 +2,21 @@ package de.tubs.ibr.dtn.sharebox.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import de.tubs.ibr.dtn.sharebox.R;
 import de.tubs.ibr.dtn.sharebox.data.Download;
+import de.tubs.ibr.dtn.sharebox.data.Utils;
 
 public class DownloadItem extends RelativeLayout {
     
     private Download mDownload = null;
+    
+    private TextView mLabel = null;
+    private TextView mBottomText = null;
+    private ImageView mIcon = null;
+    private TextView mSideText = null;
 
     public DownloadItem(Context context) {
         super(context);
@@ -15,6 +24,20 @@ public class DownloadItem extends RelativeLayout {
 
     public DownloadItem(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+    
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        mLabel = (TextView) findViewById(R.id.label);
+        mBottomText = (TextView) findViewById(R.id.bottomtext);
+        mIcon = (ImageView) findViewById(R.id.icon);
+        mSideText = (TextView) findViewById(R.id.sidetext);
+    }
+    
+    public Download getObject() {
+        return mDownload;
     }
     
     public void bind(Download d, int position) {
@@ -28,6 +51,11 @@ public class DownloadItem extends RelativeLayout {
     }
     
     private void onDataChanged() {
+        String timestamp = Utils.formatTimeStampString(getContext(), mDownload.getTimestamp().getTime());
         
+        mLabel.setText(mDownload.getSource());
+        mBottomText.setText(timestamp);
+        
+        mSideText.setText(String.valueOf(mDownload.getLength()));
     }
 }
