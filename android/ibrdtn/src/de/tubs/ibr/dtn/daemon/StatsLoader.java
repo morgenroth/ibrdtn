2cf9c6d8b31d832526a884723d6cf249b93589cc
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-06-18 14:43:22 +0200

    android-daemon: Update stats every second

--- android/ibrdtn/src/de/tubs/ibr/dtn/daemon/StatsLoader.java
+++ android/ibrdtn/src/de/tubs/ibr/dtn/daemon/StatsLoader.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.support.v4.content.AsyncTaskLoader;
 import de.tubs.ibr.dtn.service.DaemonService;
 import de.tubs.ibr.dtn.swig.NativeStats;
@@ -13,6 +14,7 @@ public class StatsLoader extends AsyncTaskLoader<NativeStats> {
     private DaemonService mService = null;
     private Boolean mStarted = false;
     private NativeStats mData = null;
+    private Handler mHandler = null;
 
     public StatsLoader(Context context, DaemonService service) {
         super(context);
@@ -39,6 +41,8 @@ public class StatsLoader extends AsyncTaskLoader<NativeStats> {
         onStopLoading();
         
         if (mStarted) {
+            mHandler.removeCallbacks(_update);
+            
             // unregister from intent receiver
             getContext().unregisterReceiver(_receiver);
             mStarted = false;
@@ -50,6 +54,9 @@ public class StatsLoader extends AsyncTaskLoader<NativeStats> {
         if (mData != null) {
             this.deliverResult(mData);
         }
+        
+        mHandler = new Handler();
+        mHandler.postDelayed(_update, 1000);
         
         IntentFilter filter = new IntentFilter(de.tubs.ibr.dtn.Intent.EVENT);
         filter.addCategory(Intent.CATEGORY_DEFAULT);
@@ -70,6 +77,16 @@ public class StatsLoader extends AsyncTaskLoader<NativeStats> {
     public NativeStats loadInBackground() {
         return mService.getStats();
     }
+    
+    private Runnable _update = new Runnable() {
+        @Override
+        public void run() {
+            onContentChanged();
+            
+            if (mStarted)
+                mHandler.postDelayed(_update, 1000);
+        }
+    };
 
     private BroadcastReceiver _receiver = new BroadcastReceiver() {
         @Override
