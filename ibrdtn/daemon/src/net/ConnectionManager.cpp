62049de718478cb37ff0400d3081f5974aa0fd26
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-08-16 18:04:40 +0200

    daemon: Several locks moved and replaced in ConnectionManager to avoid dead-lock

--- ibrdtn/daemon/src/net/ConnectionManager.cpp
+++ ibrdtn/daemon/src/net/ConnectionManager.cpp
@@ -391,7 +391,7 @@ namespace dtn
 			if (_next_autoconnect < dtn::utils::Clock::getTime())
 			{
 				// search for non-connected but available nodes
-				ibrcommon::MutexLock l(_cl_lock);
+				ibrcommon::MutexLock l(_node_lock);
 				for (nodemap::const_iterator iter = _nodes.begin(); iter != _nodes.end(); ++iter)
 				{
 					const Node &n = (*iter).second;
@@ -420,18 +420,21 @@ namespace dtn
 
 		void ConnectionManager::open(const dtn::core::Node &node) throw (ibrcommon::Exception)
 		{
-			ibrcommon::MutexLock l(_cl_lock);
-
-			// search for the right cl
-			for (std::set<ConvergenceLayer*>::iterator iter = _cl.begin(); iter != _cl.end(); ++iter)
+			// lock convergence layers while iterating over them
 			{
-				ConvergenceLayer *cl = (*iter);
-				if (node.has(cl->getDiscoveryProtocol()))
-				{
-					cl->open(node);
+				ibrcommon::MutexLock l(_cl_lock);
 
-					// stop here, we queued the bundle already
-					return;
+				// search for the right cl
+				for (std::set<ConvergenceLayer*>::iterator iter = _cl.begin(); iter != _cl.end(); ++iter)
+				{
+					ConvergenceLayer *cl = (*iter);
+					if (node.has(cl->getDiscoveryProtocol()))
+					{
+						cl->open(node);
+
+						// stop here, we queued the bundle already
+						return;
+					}
 				}
 			}
 
@@ -450,8 +453,6 @@ namespace dtn
 		void ConnectionManager::dialup(const dtn::core::Node &n)
 		{
 			// search for p2p_dialup connections
-			ibrcommon::MutexLock l(_cl_lock);
-
 			// get the list of all available URIs
 			std::list<Node::URI> uri_list = n.get(Node::NODE_P2P_DIALUP);
 
@@ -475,8 +476,6 @@ namespace dtn
 
 		void ConnectionManager::queue(const dtn::core::Node &node, const dtn::net::BundleTransfer &job)
 		{
-			ibrcommon::MutexLock l(_cl_lock);
-
 			// get the list of all available URIs
 			std::list<Node::URI> uri_list = node.getAll();
 
@@ -484,6 +483,9 @@ namespace dtn
 			for (std::list<Node::URI>::const_iterator it = uri_list.begin(); it != uri_list.end(); ++it)
 			{
 				const Node::URI &uri = (*it);
+
+				// lock convergence layers while iterating over them
+				ibrcommon::MutexLock l(_cl_lock);
 
 				// search a matching convergence layer for this URI
 				for (std::set<ConvergenceLayer*>::iterator iter = _cl.begin(); iter != _cl.end(); ++iter)
