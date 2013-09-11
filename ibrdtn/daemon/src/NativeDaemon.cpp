b17a56298f0ce1493eb0aa2e307ce925c25ff800
David Goltzsche <goltzsch@ibr.cs.tu-bs.de>
2013-09-11 12:19:02 +0200

    restored order of execution in Nativedaemon

--- ibrdtn/daemon/src/NativeDaemon.cpp
+++ ibrdtn/daemon/src/NativeDaemon.cpp
@@ -833,6 +833,12 @@ namespace dtn
 
 		void NativeDaemon::init_core() throw (NativeDaemonException)
 		{
+			// enable link manager
+			ibrcommon::LinkManager::initialize();
+
+			// get the core
+			dtn::core::BundleCore &core = dtn::core::BundleCore::getInstance();
+
 			// reset and get configuration
 			dtn::daemon::Configuration &conf = dtn::daemon::Configuration::getInstance();
 
@@ -845,13 +851,9 @@ namespace dtn
 				conf.load();
 			}
 
-			// enable and configure link manager
+			//configure link manager
 			size_t interval = conf.getNetwork().getLinkRequestInterval();
 			ibrcommon::LinkManager::setLinkRequestInterval(interval);
-			ibrcommon::LinkManager::initialize();
-
-			// get the core
-			dtn::core::BundleCore &core = dtn::core::BundleCore::getInstance();
 
 			// greeting
 			if (conf.getDebug().enabled())
