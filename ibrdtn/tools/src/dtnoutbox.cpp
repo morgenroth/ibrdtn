93d9e352ad96240752c1269d54573519afb0c60b
David Goltzsche <goltzsch@ibr.cs.tu-bs.de>
2013-09-13 17:49:57 +0200

    dtnoutbox: keep track of files in folder

--- ibrdtn/tools/src/dtnoutbox.cpp
+++ ibrdtn/tools/src/dtnoutbox.cpp
@@ -46,51 +46,70 @@ using namespace ibrcommon;
 
 void print_help()
 {
-        cout << "-- dtnoutbox (IBR-DTN) --" << endl;
-        cout << "Syntax: dtnoutbox [options] <name> <outbox> <destination>"  << endl;
-        cout << " <name>           the application name" << endl;
-        cout << " <outbox>         directory with outgoing files" << endl;
-        cout << " <destination>    the destination EID for all outgoing files" << endl;
-        cout << "* optional parameters *" << endl;
-        cout << " -h|--help        display this text" << endl;
-        cout << " -w|--workdir     temporary work directory" << endl;
-        cout << " -k|--keep        keep files in outbox" << endl;
+	cout << "-- dtnoutbox (IBR-DTN) --" << endl;
+	cout << "Syntax: dtnoutbox [options] <name> <outbox> <destination>" << endl;
+	cout << " <name>           the application name" << endl;
+	cout << " <outbox>         directory with outgoing files" << endl;
+	cout << " <destination>    the destination EID for all outgoing files" << endl;
+	cout << "* optional parameters *" << endl;
+	cout << " -h|--help        display this text" << endl;
+	cout << " -w|--workdir     temporary work directory" << endl;
+	cout << " -k|--keep        keep files in outbox" << endl;
+	cout << " -i <interval>	   interval in milliseconds, in which <outbox> is scanned for new/changed files. default: 5000" << endl;
+	cout << " -n <number>	   number of intervals, after which a unchanged file is considered as written. default: 3" << endl;
+
 }
 
-map<string,string> readconfiguration(int argc, char** argv)
+map<string,string> readconfiguration( int argc, char** argv )
 {
-    // print help if not enough parameters are set
-    if (argc < 4) { print_help(); exit(0); }
+	// print help if not enough parameters are set
+	if (argc < 4)
+	{
+		print_help();
+		exit(0);
+	}
 
-    map<string,string> ret;
-    ret["name"] = argv[1];
-    ret["outbox"] = argv[2];
-    ret["destination"] = argv[3];
+	map<string,string> ret;
+	ret["name"] = argv[1];
+	ret["outbox"] = argv[2];
+	ret["destination"] = argv[3];
+	ret["interval"] = "5000"; //default value
+	ret["rounds"] = "3"; //default value
 
-    for (int i = 4; i < argc; ++i)
-    {
-        string arg = argv[i];
+	for (int i = 4; i < argc; ++i)
+	{
+		string arg = argv[i];
 
-        // print help if requested
-        if (arg == "-h" || arg == "--help")
-        {
-            print_help();
-            exit(0);
-        }
+		// print help if requested
+		if (arg == "-h" || arg == "--help")
+		{
+			print_help();
+			exit(0);
+		}
 
-        if (arg == "-w" || arg == "--workdir")
-        {
-            ret["workdir"] = argv[i + 1];
-        }
+		if (arg == "-w" || arg == "--workdir")
+		{
+			ret["workdir"] = argv[++i];
+		}
 
-        if (arg == "-k" || arg == "--keep")
-        {
-            ret["keep"] = "1";
-        }
+		if (arg == "-k" || arg == "--keep")
+		{
+			ret["keep"] = "1";
+		}
 
-    }
+		if (arg == "-i")
+		{
+			ret["interval"] = argv[++i];
+		}
 
-    return ret;
+		if (arg == "-n")
+		{
+			ret["rounds"] = argv[++i];
+		}
+
+	}
+
+	return ret;
 }
 
 // set this variable to false to stop the app
@@ -99,232 +118,290 @@ bool _running = true;
 // global connection
 ibrcommon::socketstream *_conn = NULL;
 
-void term(int signal)
+void term( int signal )
 {
-    if (signal >= 1)
-    {
-        _running = false;
-        if (_conn != NULL) _conn->close();
-    }
+	if (signal >= 1)
+	{
+		_running = false;
+		if (_conn != NULL) _conn->close();
+	}
 }
 
-void
-write_archive(std::string out_path, const char **filename, size_t num_files)
+/*
+ * #### CALLBACKS FOR LIBARCHIVE ####
+ */
+int my_open_callback( struct archive *, void *blob_iostream )
 {
-  struct archive *a;
-  struct archive_entry *entry;
-  struct stat st;
-  char buff[8192];
-  int len;
-  int fd;
-
-  struct timespec ts;
- clock_gettime(CLOCK_REALTIME, &ts);
-
-  a = archive_write_new();
-  archive_write_set_format_ustar(a);
-  archive_write_open_filename(a, out_path.c_str());
-  while (num_files != 0) {
-
-    stat(*filename, &st);
-    entry = archive_entry_new();
-    archive_entry_set_size(entry, st.st_size);
-    archive_entry_set_filetype(entry, AE_IFREG);
-    archive_entry_set_perm(entry, 0644);
-
-    //set filename in archive to relative paths
-    std::string path(*filename);
-    unsigned slash_pos = path.find_last_of('/',path.length());
-    std::string rel_name = path.substr(slash_pos+1,path.length() - slash_pos);
-    archive_entry_set_pathname(entry, rel_name.c_str());
-
-    //set timestamps
-    archive_entry_set_atime(entry, ts.tv_sec, ts.tv_nsec); //accesstime
-    archive_entry_set_birthtime(entry, ts.tv_sec, ts.tv_nsec); //creationtime
-    archive_entry_set_ctime(entry, ts.tv_sec, ts.tv_nsec); //time, inode changed
-    archive_entry_set_mtime(entry, ts.tv_sec, ts.tv_nsec); //modification time
-
-    archive_write_header(a, entry);
-    fd = open(*filename, O_RDONLY);
-    len = read(fd, buff, sizeof(buff));
-    while ( len > 0 ) {
-        archive_write_data(a, buff, len);
-        len = read(fd, buff, sizeof(buff));
-    }
-    close(fd);
-    archive_entry_free(entry);
-    filename++;
-    num_files--;
-  }
-  archive_write_close(a);
-  archive_write_free(a);
+	return ARCHIVE_OK;
+}
+ssize_t my_write_callback( struct archive *, void *blob_ptr, const void *buffer, size_t length )
+{
+	char* cast_buf = (char*) buffer;
+	BLOB::Reference blob = *((BLOB::Reference*) blob_ptr);
+	BLOB::iostream s = blob.iostream();
+	(*s).write(cast_buf,length);
+	return length;
 }
 
+int my_close_callback( struct archive *, void *blob_iostream )
+{
+	return ARCHIVE_OK;
+}
+/*
+ * #### END CALLBACKS FOR LIBARCHIVE ####
+ */
+void write_archive( ibrcommon::BLOB::Reference *blob, const char **filename, size_t num_files )
+{
+	struct archive *a;
+	struct archive_entry *entry;
+	struct stat st;
+	char buff[8192];
+	int len;
+	int fd;
+
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+
+	//create new archive, set format to tar, use callbacks (above this method)
+	a = archive_write_new();
+	archive_write_set_format_ustar(a);
+	archive_write_open(a, blob, &my_open_callback, &my_write_callback, my_close_callback);
+
+	while (num_files != 0)
+	{
+
+		stat(*filename, &st);
+		entry = archive_entry_new();
+		archive_entry_set_size(entry, st.st_size);
+		archive_entry_set_filetype(entry, AE_IFREG);
+		archive_entry_set_perm(entry, 0644);
+
+		//set filename in archive to relative paths
+		std::string path(*filename);
+		unsigned slash_pos = path.find_last_of('/', path.length());
+		std::string rel_name = path.substr(slash_pos + 1, path.length() - slash_pos);
+		archive_entry_set_pathname(entry, rel_name.c_str());
+
+		//set timestamps
+		archive_entry_set_atime(entry, ts.tv_sec, ts.tv_nsec); //accesstime
+		archive_entry_set_birthtime(entry, ts.tv_sec, ts.tv_nsec); //creationtime
+		archive_entry_set_ctime(entry, ts.tv_sec, ts.tv_nsec); //time, inode changed
+		archive_entry_set_mtime(entry, ts.tv_sec, ts.tv_nsec); //modification time
+
+		archive_write_header(a, entry);
+		fd = open(*filename, O_RDONLY);
+		len = read(fd, buff, sizeof(buff));
+		while (len > 0)
+		{
+			archive_write_data(a, buff, len);
+			len = read(fd, buff, sizeof(buff));
+		}
+		close(fd);
+		archive_entry_free(entry);
+		filename++;
+		num_files--;
+	}
+	archive_write_close(a);
+	archive_write_free(a);
+}
 
 /*
  * main application method
  */
-int main(int argc, char** argv)
+int main( int argc, char** argv )
 {
 	bool keep_files = false;
-    // catch process signals
-    signal(SIGINT, term);
-    signal(SIGTERM, term);
+	// catch process signals
+	signal(SIGINT, term);
+	signal(SIGTERM, term);
 
-    // read the configuration
-    map<string,string> conf = readconfiguration(argc, argv);
+	// read the configuration
+	map<string,string> conf = readconfiguration(argc, argv);
+	size_t _conf_interval = atoi(conf["interval"].c_str());
+	size_t _conf_rounds = atoi(conf["rounds"].c_str());
+	size_t _conf_stable_interval = _conf_interval * _conf_rounds / 1000; //in seconds!
 
-    // init working directory
-    if (conf.find("workdir") != conf.end())
-    {
-    	ibrcommon::File blob_path(conf["workdir"]);
+	// init working directory
+	if (conf.find("workdir") != conf.end())
+	{
+		ibrcommon::File blob_path(conf["workdir"]);
 
-    	if (blob_path.exists())
-    	{
-    		ibrcommon::BLOB::changeProvider(new ibrcommon::FileBLOBProvider(blob_path), true);
-    	}
-    }
+		if (blob_path.exists())
+		{
+			ibrcommon::BLOB::changeProvider(new ibrcommon::FileBLOBProvider(blob_path), true);
+		}
+	}
 
-    //check keep parameter
-    if (conf.find("keep") != conf.end())
+	//check keep parameter
+	if (conf.find("keep") != conf.end())
 	{
 		keep_files = true;
 	}
 
-    // backoff for reconnect
-    unsigned int backoff = 2;
+	// backoff for reconnect
+	unsigned int backoff = 2;
 
-    // check outbox for files
+	// check outbox for files
 	File outbox(conf["outbox"]);
 
-    // loop, if no stop if requested
-    while (_running)
-    {
-        try {
-        	// Create a stream to the server using TCP.
-        	ibrcommon::vaddress addr("localhost", 4550);
-        	ibrcommon::socketstream conn(new ibrcommon::tcpsocket(addr));
+	// loop, if no stop if requested
+	while (_running)
+	{
+		try
+		{
+			// Create a stream to the server using TCP.
+			ibrcommon::vaddress addr("localhost", 4550);
+			ibrcommon::socketstream conn(new ibrcommon::tcpsocket(addr));
 
-        	// set the connection globally
-        	_conn = &conn;
+			// set the connection globally
+			_conn = &conn;
 
-            // Initiate a client for synchronous receiving
-            dtn::api::Client client(conf["name"], conn, dtn::api::Client::MODE_SENDONLY);
+			// Initiate a client for synchronous receiving
+			dtn::api::Client client(conf["name"], conn, dtn::api::Client::MODE_SENDONLY);
 
-            // Connect to the server. Actually, this function initiate the
-            // stream protocol by starting the thread and sending the contact header.
-            client.connect();
+			// Connect to the server. Actually, this function initiate the
+			// stream protocol by starting the thread and sending the contact header.
+			client.connect();
 
-            // reset backoff if connected
-            backoff = 2;
+			// reset backoff if connected
+			backoff = 2;
 
-            // check the connection
-            while (_running)
-            {
-            	list<File> files;
-            	outbox.getFiles(files);
+			//map of sent files with their timestamp
+			map<std::string,size_t> files_sent;
+			//TODO files wieder löschen, wenn vom benutzer gelöscht?
 
-            	// <= 2 because of "." and ".."
-            	if (files.size() <= 2)
-            	{
-                    // wait some seconds
-            		ibrcommon::Thread::sleep(10000); //TODO konfigurierbar machen?
+			//map of files with a counter how often they were scanned
+			map<std::string,size_t> files_seen;
 
-                    continue;
-            	}
 
-            	stringstream file_list;
-            	size_t num_files = files.size()-2; //-2 because of . and ..
-            	size_t prefix_length = 11;
-            	const char **file_list_ptr = new const char*[num_files];
-            	size_t counter = 0;
-            	for (list<File>::iterator iter = files.begin(); iter != files.end(); ++iter)
-            	{
+			// check the connection
+			while (_running)
+			{
+				list<File> files;
+				outbox.getFiles(files);
+
+				// <= 2 because of "." and ".."
+				if (files.size() <= 2)
+				{
+					cout << "no files to send " << endl;
+					// wait some seconds
+					ibrcommon::Thread::sleep(_conf_interval);
+
+					continue;
+				}
+
+				stringstream file_list;
+				const char **file_list_ptr = new const char*[files.size() -2]; //-2 because of . and ..
+				size_t counter = 0;
+				for (list<File>::iterator iter = files.begin(); iter != files.end(); ++iter)
+				{
 					File &f = (*iter);
 
 					// skip system files ("." and "..")
 					if (f.isSystem()) continue;
 
-					file_list_ptr[counter] = new char[100];
-					file_list_ptr[counter] = f.getPath().c_str();
-					counter++;
 
-					// add the file to the filelist
-					file_list << f.getBasename() << " ";
-            	}
+					// add the file to the filelist, if it has not been sent (with the latest timestamp)
+					size_t latest_timestamp = max(f.lastmodify(),f.laststatchange());
 
-            	// output of all files to send
-            	cout << "files: " << file_list.str() << endl;
+					//only consider files with a new timestamp
+					if(files_sent[f.getPath()] < latest_timestamp)
+					{
+						//TODO größe betrachten!!!
+						time_t  timev;
+						time(&timev);
+						cout << latest_timestamp << "+" << _conf_stable_interval << endl;
+						cout << latest_timestamp + _conf_stable_interval << "<=" << timev << endl;
+						if(latest_timestamp + _conf_stable_interval <= timev)
+						{
+							cout << "true" << endl;
+							files_sent[f.getPath()] = latest_timestamp;
 
+							file_list << f.getBasename() << " ";
+							file_list_ptr[counter++] = f.getPath().c_str();
+						}
+					}
+				}
+
+
+				if(!counter)
+				{
+					cout << "no files to send " << endl;
+				}
+				else
+				{
+					cout << "files: " << file_list.str() << endl;
+
+				// create a blob
+				ibrcommon::BLOB::Reference blob = ibrcommon::BLOB::create();
 #ifdef HAVE_LIBARCHIVE
-            	write_archive("/tmp/test.tar",file_list_ptr,num_files);
 
-            	//delete files, if wanted
-            	if(!keep_files)
-            	{
-            		list<File>::iterator file_iter = files.begin();
-            		while(file_iter != files.end())
-            		{
-            			(*file_iter++).remove(false);
-            		}
-            	}
-            	ifstream stream;
-            	stream.open("/tmp/test.tar", std::ifstream::in);
+					write_archive(&blob, file_list_ptr, counter);
+
+					//delete files, if wanted
+					if (!keep_files)
+					{
+						list<File>::iterator file_iter = files.begin();
+						while (file_iter != files.end())
+						{
+							(*file_iter++).remove(false);
+						}
+					}
 #else
-            	// "--remove-files" deletes files after adding
-            	//depending on configuration, this option is passed to tar or not
-            	std::string remove_string = " --remove-files";
-            	if(keep_files)
-            		remove_string = "";
-            	stringstream cmd;
-            	cmd << "tar" << remove_string << " -cO -C " << outbox.getPath() << " " << file_list.str();
+//use commandline fallback
+					// "--remove-files" deletes files after adding
+					//depending on configuration, this option is passed to tar or not
+					std::string remove_string = " --remove-files";
+					if(keep_files)
+					remove_string = "";
+					stringstream cmd;
+					cmd << "tar" << remove_string << " -cO -C " << outbox.getPath() << " " << file_list.str();
 
-            	// make a tar archive
-            	appstreambuf app(cmd.str(), appstreambuf::MODE_READ);
-            	istream stream(&app);
+					// make a tar archive
+					appstreambuf app(cmd.str(), appstreambuf::MODE_READ);
+					istream stream(&app);
 
+					// stream the content of "tar" to the payload block
+					(*blob.iostream()) << stream.rdbuf();
 #endif
-    			// create a blob
-            	ibrcommon::BLOB::Reference blob = ibrcommon::BLOB::create();
+					// create a new bundle
+					dtn::data::EID destination = EID(conf["destination"]);
 
-    			// stream the content of "tar" to the payload block
-    			(*blob.iostream()) << stream.rdbuf();
-            	// create a new bundle
-    			dtn::data::EID destination = EID(conf["destination"]);
+					// create a new bundle
+					dtn::data::Bundle b;
 
-    			// create a new bundle
-    			dtn::data::Bundle b;
+					// set destination
+					b.destination = destination;
 
-    			// set destination
-    			b.destination = destination;
+					// add payload block using the blob
+					b.push_back(blob);
 
-    			// add payload block using the blob
-    			b.push_back(blob);
-
-                // send the bundle
-    			client << b; client.flush();
-
-            	if (_running)
-            	{
+					// send the bundle
+					client << b;
+					client.flush();
+				}
+				if (_running)
+				{
 					// wait some seconds
-            		ibrcommon::Thread::sleep(10000);
-            	}
-            }
+					ibrcommon::Thread::sleep(_conf_interval);
+				}
+			}
 
-            // close the client connection
-            client.close();
+			// close the client connection
+			client.close();
 
-            // close the connection
-            conn.close();
+			// close the connection
+			conn.close();
 
-            // set the global connection to NULL
-            _conn = NULL;
-        } catch (const ibrcommon::socket_exception&) {
-        	// set the global connection to NULL
-        	_conn = NULL;
+			// set the global connection to NULL
+			_conn = NULL;
+		} catch (const ibrcommon::socket_exception&)
+		{
+			// set the global connection to NULL
+			_conn = NULL;
 
-        	if (_running)
-        	{
+			if (_running)
+			{
 				cout << "Connection to bundle daemon failed. Retry in " << backoff << " seconds." << endl;
 				ibrcommon::Thread::sleep(backoff * 1000);
 
@@ -334,13 +411,14 @@ int main(int argc, char** argv)
 					// set a new backoff
 					backoff = backoff * 2;
 				}
-        	}
-        } catch (const ibrcommon::IOException&) {
-        	// set the global connection to NULL
-        	_conn = NULL;
+			}
+		} catch (const ibrcommon::IOException&)
+		{
+			// set the global connection to NULL
+			_conn = NULL;
 
-        	if (_running)
-        	{
+			if (_running)
+			{
 				cout << "Connection to bundle daemon failed. Retry in " << backoff << " seconds." << endl;
 				ibrcommon::Thread::sleep(backoff * 1000);
 
@@ -350,12 +428,13 @@ int main(int argc, char** argv)
 					// set a new backoff
 					backoff = backoff * 2;
 				}
-        	}
-    	} catch (const std::exception&) {
-        	// set the global connection to NULL
-        	_conn = NULL;
-    	}
-    }
+			}
+		} catch (const std::exception&)
+		{
+			// set the global connection to NULL
+			_conn = NULL;
+		}
+	}
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
