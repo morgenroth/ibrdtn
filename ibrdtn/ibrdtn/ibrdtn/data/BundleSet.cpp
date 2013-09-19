cba01a346a1726ef34e22257ae6127d243ac9105
David Goltzsche <goltzsch@ibr.cs.tu-bs.de>
2013-09-19 11:58:45 +0200

    ibrdtn: removed setPath-call from BundleSet::Factory

--- ibrdtn/ibrdtn/ibrdtn/data/BundleSet.cpp
+++ ibrdtn/ibrdtn/ibrdtn/data/BundleSet.cpp
@@ -29,7 +29,6 @@ namespace dtn
 {
 	namespace data
 	{
-		ibrcommon::File __path("/tmp/bundles"); //TODO where configure this?
 		BundleSet::Factory* BundleSet::__factory__ = NULL;
 
 		void BundleSet::setFactory(dtn::data::BundleSet::Factory *f)
@@ -65,9 +64,7 @@ namespace dtn
 			}
 
 			// by default, return a memory bundle-set
-			MemoryBundleSet* mbs = new MemoryBundleSet(name, listener, bf_size);
-			mbs->setPath(__path);
-			return mbs;
+			return new MemoryBundleSet(name, listener, bf_size);
 		}
 
 		BundleSet::BundleSet(BundleSet::Listener *listener, Size bf_size)
