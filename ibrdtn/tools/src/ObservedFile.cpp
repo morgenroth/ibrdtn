3e72ead6a4f2afd56264cacbebdf157ada960edd
David Goltzsche <goltzsch@ibr.cs.tu-bs.de>
2013-09-23 15:58:44 +0200

    ibrdtn tools: created ObservedFile

---
+++ ibrdtn/tools/src/ObservedFile.cpp
@@ -0,0 +1,76 @@
+#include "ObservedFile.h"
+					//size_t latest_timestamp = max(of.lastmodify(), of.laststatchange());
+
+template<typename T>
+inline ObservedFile<T>::ObservedFile( std::string path ) : _last_sent(-1)
+{
+}
+
+template<typename T>
+inline ObservedFile<T>::~ObservedFile()
+{
+}
+
+template<typename T>
+inline int ObservedFile<T>::getFiles( list<T> files )
+{
+	_file.getFiles(files);
+}
+
+template<typename T>
+inline string ObservedFile<T>::getPath()
+{
+	return _file.getPath();
+}
+
+template<typename T>
+inline bool ObservedFile<T>::exists()
+{
+	return _file.exists;
+}
+
+template<typename T>
+inline time_t ObservedFile<T>::getLastTimestamp()
+{
+	return  max(_file.lastmodify(),_file.laststatchange());
+}
+
+template<typename T>
+inline string ObservedFile<T>::getBasename()
+{
+	return _file.getBasename();
+}
+
+template<typename T>
+inline bool ObservedFile<T>::lastSizesEqual( size_t n )
+{
+	if (n > _sizes.size()) return false;
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		if (_sizes.at(_sizes.size() - i) != _sizes.at(_sizes.size() - i - 1)) return false;
+	}
+	return true;
+}
+
+template<typename T>
+inline void ObservedFile<T>::send()
+{
+	time_t tm;
+	_last_sent = time(&tm);
+	_sizes.clear();
+}
+
+template<typename T>
+inline void ObservedFile<T>::addSize()
+{
+	_sizes.push_back(_file.size());
+}
+
+
+
+template<typename T>
+inline size_t ObservedFile<T>::getLastSent()
+{
+	return _last_sent;
+}
---
+++ ibrdtn/tools/src/ObservedFile.h
@@ -0,0 +1,43 @@
+/*
+ * ObservedFile.h
+ *
+ *  Created on: Sep 20, 2013
+ *      Author: goltzsch
+ */
+
+#ifndef OBSERVEDFILE_H_
+#define OBSERVEDFILE_H_
+
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class ObservedFile
+{
+public:
+	ObservedFile(std::string path);
+	virtual ~ObservedFile();
+
+	int getFiles(list<T> files);
+	string getPath();
+	bool exists();
+	string getBasename();
+
+	time_t getLastTimestamp();
+	bool lastSizesEqual( size_t n );
+	void send();
+	void addSize();
+	size_t getLastSent();
+
+private:
+	T _file;
+	size_t _last_sent;
+	std::vector<size_t> _sizes;
+};
+
+
+
+#endif /* OBSERVEDFILE_H_ */
