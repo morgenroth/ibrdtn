31e86196d39d631ba120a10a6027633bf19fd15a
David Goltzsche <david@goltzsche.net>
2013-10-14 16:44:42 +0200

    ibrdtn tools: removec useless comment and import

--- ibrdtn/tools/src/ObservedFile.cpp
+++ ibrdtn/tools/src/ObservedFile.cpp
@@ -21,32 +21,9 @@
  */
 
 #include "ObservedFile.h"
-#include <stdio.h>
 
 string ObservedFile::_conf_imgpath = "";
 size_t ObservedFile::_conf_rounds = 0;
-/*
- * ObservedFile.h
- *
- * Copyright (C) 2013 IBR, TU Braunschweig
- *
- * Written-by: David Goltzsche <goltzsch@ibr.cs.tu-bs.de>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *  Created on: Sep 23, 2013
- */
-
 
 ObservedFile::ObservedFile() : _last_sent(0)
 {
