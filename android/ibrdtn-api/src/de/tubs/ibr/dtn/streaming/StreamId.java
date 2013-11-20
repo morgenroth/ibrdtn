1b763f116bbf5cabbb68b312422cad3648e635b0
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-11-20 10:54:16 +0100

    android-api: Add toString(), equals() and hashCode() to StreamId

--- android/ibrdtn-api/src/de/tubs/ibr/dtn/streaming/StreamId.java
+++ android/ibrdtn-api/src/de/tubs/ibr/dtn/streaming/StreamId.java
@@ -5,4 +5,28 @@ import de.tubs.ibr.dtn.api.SingletonEndpoint;
 public class StreamId {
     public SingletonEndpoint source = null;
     public int correlator = 0;
+    
+    @Override
+    public String toString() {
+        return source + "#" + Integer.valueOf(correlator);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof StreamId) {
+            StreamId other = (StreamId)o;
+            if (other.correlator == correlator) {
+                if ((source == null) && (other.source == null)) return true;
+                return source.equals(other.source);
+            }
+            return false;
+        }
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        if (source == null) return correlator;
+        return source.hashCode() ^ correlator;
+    }
 }
