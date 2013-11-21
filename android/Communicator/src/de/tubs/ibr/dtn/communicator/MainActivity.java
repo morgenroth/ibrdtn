b88be5275d15b0f91e58abb04dd12ea8a9be7fd2
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-11-21 10:37:09 +0100

    Communicator: add chirp sound and transmission animation

--- android/Communicator/src/de/tubs/ibr/dtn/communicator/MainActivity.java
+++ android/Communicator/src/de/tubs/ibr/dtn/communicator/MainActivity.java
@@ -1,23 +1,36 @@
 
 package de.tubs.ibr.dtn.communicator;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ToggleButton;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
     
     private static final String TAG = "MainActivity";
-    private ToggleButton mToggle = null;
+    private ImageButton mToggle = null;
+    private FrameLayout mTransmissionIndicator = null;
+    private Animation mTransmissionAnim = null;
+    
+    private boolean mActivated = false;
+    private SoundPool mSounds = null;
+    private int mChirpSound = 0;
     
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
@@ -36,17 +49,43 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        mToggle = (ToggleButton)findViewById(R.id.toggleButton);
+        // load sound pool
+        mSounds = new SoundPool(1, AudioManager.STREAM_SYSTEM, 0);
+        try {
+            mChirpSound = mSounds.load(getAssets().openFd("chirp.mp3"), 1);
+        } catch (IOException e) {
+            Log.e(TAG, "sound loading failed.", e);
+        }
+        
+        mTransmissionIndicator = (FrameLayout)findViewById(R.id.indicator_transmission);
+        mTransmissionIndicator.setVisibility(View.INVISIBLE);
+        
+        mTransmissionAnim = AnimationUtils.loadAnimation(this, R.anim.transmission);
+        
+        mToggle = (ImageButton)findViewById(R.id.button_transmission);
         mToggle.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mToggle.isChecked()) {
+                if (!mActivated) {
+                    mActivated = true;
                     Log.d(TAG, "start recording");
+                    
+                    // play chirp sound
+                    mSounds.play(mChirpSound, 1.0f, 1.0f, 1, 0, 1.0f);
+                    
+                    mTransmissionIndicator.startAnimation(mTransmissionAnim);
+                    mTransmissionIndicator.setVisibility(View.VISIBLE);
+                    
                     Intent i = new Intent(MainActivity.this, CommService.class);
                     i.setAction(CommService.OPEN_COMM_CHANNEL);
                     startService(i);
                 } else {
+                    mActivated = false;
                     Log.d(TAG, "stop recording");
+                    
+                    mTransmissionIndicator.clearAnimation();
+                    mTransmissionIndicator.setVisibility(View.INVISIBLE);
+                    
                     Intent i = new Intent(MainActivity.this, CommService.class);
                     i.setAction(CommService.CLOSE_COMM_CHANNEL);
                     startService(i);
@@ -72,5 +111,11 @@ public class MainActivity extends Activity {
     protected void onStop() {
         unbindService(mServiceConnection);
         super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mSounds != null) mSounds.release();
+        super.onDestroy();
     }
 }
