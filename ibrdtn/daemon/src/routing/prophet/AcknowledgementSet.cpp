d3e639e8904f76f40d76b952a7f83b54f3bfb642
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-01-09 09:25:41 +0100

    daemon: Encode the lifetime of bundles into the prophet acknowledgement set

--- ibrdtn/daemon/src/routing/prophet/AcknowledgementSet.cpp
+++ ibrdtn/daemon/src/routing/prophet/AcknowledgementSet.cpp
@@ -90,6 +90,7 @@ namespace dtn
 				const dtn::data::MetaBundle &ack = (*it);
 				stream << (const dtn::data::BundleID&)ack;
 				ack.expiretime.encode(stream);
+				ack.lifetime.encode(stream);
 			}
 
 			return stream;
@@ -108,7 +109,7 @@ namespace dtn
 				dtn::data::MetaBundle ack;
 				stream >> (dtn::data::BundleID&)ack;
 				ack.expiretime.decode(stream);
-				ack.lifetime = dtn::utils::Clock::getLifetime(ack, ack.expiretime);
+				ack.lifetime.decode(stream);
 
 				ack_set.add(ack);
 			}
