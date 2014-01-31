bc2f8b9e860c29a542c2bed971fd7e21ab15e39d
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-01-31 19:01:34 +0100

    android-daemon: Implement DaemonManager API

---
+++ android/ibrdtn/src/de/tubs/ibr/dtn/service/DaemonManager.java
@@ -0,0 +1,38 @@
+package de.tubs.ibr.dtn.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.preference.PreferenceManager;
+import de.tubs.ibr.dtn.DtnManager;
+
+public class DaemonManager extends Service {
+    
+    private final DtnManager.Stub mBinder = new DtnManager.Stub() {
+        @Override
+        public void setDtnEnabled(boolean state) throws RemoteException {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(DaemonManager.this);
+            prefs.edit().putBoolean("enabledSwitch", state).commit();
+            
+            if (state) {
+                // start the DTN service
+                Intent is = new Intent(DaemonManager.this, DaemonService.class);
+                is.setAction(DaemonService.ACTION_STARTUP);
+                startService(is);
+            }
+        }
+
+        @Override
+        public boolean isDtnEnabled() throws RemoteException {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(DaemonManager.this);
+            return prefs.getBoolean("enabledSwitch", false);
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
