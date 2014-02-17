adde887c16239d777b692f5310cc5e6aabbc580e
David Goltzsche <david@goltzsche.net>
2014-02-17 17:37:15 +0100

    buildroot: added new recipies

--- recipes/buildroot/ibrdtn-tools/ibrdtn-tools.mk
+++ recipes/buildroot/ibrdtn-tools/ibrdtn-tools.mk
@@ -8,9 +8,10 @@
 IBRDTN_TOOLS_SITE:=/home/goltzsch/workspace/ibrdtn-repo/ibrdtn/tools
 IBRDTN_TOOLS_SITE_METHOD:=local
 IBRDTN_TOOLS_LIBTOOL_PATCH:=NO
-IBRDTN_TOOLS_DEPENDENCIES:=ibrcommon ibrdtn libarchive
+IBRDTN_TOOLS_DEPENDENCIES:=libtffs openssl libarchive ibrcommon ibrdtn
 IBRDTN_TOOLS_INSTALL_STAGING:=YES
 IBRDTN_TOOLS_INSTALL_TARGET:=YES
+IBRDTN_TOOLS_CONF_OPT = --with-tffs=${STAGING_DIR}/usr/
 
 IBRDTN_TOOLS_MAKE_OPT += LDFLAGS+="-all-static -ldl"
 
