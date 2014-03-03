70af7a7129b72e4926fe2ee26e7ffd60f0211015
David Goltzsche <david@goltzsche.net>
2014-03-03 11:02:48 +0100

    recipes for new ibrdtn-tools

--- recipes/buildroot/ibrdtn-tools/ibrdtn-tools.mk
+++ recipes/buildroot/ibrdtn-tools/ibrdtn-tools.mk
@@ -7,8 +7,9 @@ IBRDTN_TOOLS_VERSION:=0.10.0
 IBRDTN_TOOLS_SOURCE:=ibrdtn-tools-$(IBRDTN_TOOLS_VERSION).tar.gz
 IBRDTN_TOOLS_SITE:=http://www.ibr.cs.tu-bs.de/projects/ibr-dtn/releases
 IBRDTN_TOOLS_LIBTOOL_PATCH:=NO
-IBRDTN_TOOLS_DEPENDENCIES:=ibrcommon ibrdtn
+IBRDTN_TOOLS_DEPENDENCIES:=libtffs openssl libarchive ibrcommon ibrdtn
 IBRDTN_TOOLS_INSTALL_STAGING:=YES
 IBRDTN_TOOLS_INSTALL_TARGET:=YES
+IBRDTN_TOOLS_CONF_OPT = --with-tffs=${STAGING_DIR}/usr/
 
 $(eval $(call AUTOTARGETS,package,ibrdtn-tools))
