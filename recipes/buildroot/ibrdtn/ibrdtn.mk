4b22392482b3188ff6ca30847a9cee515c1e2d9c
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-04-04 14:34:05 +0200

    recipes: Remove no longer needed configure options and dependencies

--- recipes/buildroot/ibrdtn/ibrdtn.mk
+++ recipes/buildroot/ibrdtn/ibrdtn.mk
@@ -7,12 +7,11 @@ IBRDTN_VERSION:=0.13.0
 IBRDTN_SOURCE:=ibrdtn-$(IBRDTN_VERSION).tar.gz
 IBRDTN_SITE:=http://www.ibr.cs.tu-bs.de/projects/ibr-dtn/releases
 IBRDTN_LIBTOOL_PATCH:=NO
-IBRDTN_DEPENDENCIES:=host-pkgconf ibrcommon openssl zlib
+IBRDTN_DEPENDENCIES:=host-pkgconf ibrcommon zlib
 IBRDTN_INSTALL_STAGING:=YES
 IBRDTN_INSTALL_TARGET:=YES
 
 IBRDTN_CONF_OPT =	\
-		--with-dtnsec \
 		--with-compression
 
 $(eval $(autotools-package))
--- recipes/buildroot/ibrdtnd/ibrdtnd.mk
+++ recipes/buildroot/ibrdtnd/ibrdtnd.mk
@@ -7,7 +7,7 @@ IBRDTND_VERSION:=0.13.0
 IBRDTND_SOURCE:=ibrdtnd-$(IBRDTND_VERSION).tar.gz
 IBRDTND_SITE:=http://www.ibr.cs.tu-bs.de/projects/ibr-dtn/releases
 IBRDTND_LIBTOOL_PATCH:=NO
-IBRDTND_DEPENDENCIES:=dtndht ibrcommon ibrdtn libdaemon openssl zlib sqlite
+IBRDTND_DEPENDENCIES:=dtndht ibrcommon ibrdtn libdaemon sqlite
 IBRDTND_INSTALL_STAGING:=YES
 IBRDTND_INSTALL_TARGET:=YES
 
@@ -17,8 +17,6 @@ endif
 
 IBRDTND_CONF_OPT =	\
 		--with-sqlite \
-		--with-dtnsec \
-		--with-compression \
 		--with-tls
 		
 
