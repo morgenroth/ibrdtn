bbc70f40be7655786d2ad2b59f23f76620efd550
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-06-13 18:39:09 +0200

    android-daemon: Include build-config.mk to include openssl cflags

--- android/ibrdtn/jni/openssl_Android.mk
+++ android/ibrdtn/jni/openssl_Android.mk
@@ -11,5 +11,6 @@ LOCAL_MODULE    := openssl
 # These makefiles are here instead of being Android.mk files in the
 # respective crypto, ssl, and apps directories so
 # that import_openssl.sh import won't remove them.
+include $(LOCAL_PATH)/build-config.mk
 include $(LOCAL_PATH)/Crypto.mk
 include $(LOCAL_PATH)/Ssl.mk 
