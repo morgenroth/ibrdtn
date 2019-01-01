199c86591edc7e82b92903efecadc4f69ea63370
Johannes Morgenroth <jm@m-network.de>
2019-01-01 11:25:50 +0100

    Add operator!=() to Bundle::block_elem
    Alternative implementations of libstdcpp as uclibc++ use it in
    the algorithm implemenetations.

--- ibrdtn/ibrdtn/ibrdtn/data/Bundle.h
+++ ibrdtn/ibrdtn/ibrdtn/data/Bundle.h
@@ -69,6 +69,9 @@ namespace dtn
 				bool operator==(const dtn::data::block_t &type) const {
 					return (**this) == type;
 				}
+				bool operator!=(const dtn::data::block_t &type) const {
+					return !((**this) == type);
+				}
 			};
 
 			typedef std::list<block_elem> block_list;
